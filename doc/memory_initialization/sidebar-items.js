f0f3c35923648e86a3e5ee1e8dfc1be0451a50ec
Ramla-I <Ramla-I@users.noreply.github.com>
2022-03-09 20:37:43 +0000

    Merge branch 'theseus_main' of https://github.com/theseus-os/Theseus into theseus_main 7c01d31bb9ac9a1e75b97bcb9645cccba37d7c35

---
+++ doc/memory_initialization/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["init_memory_management","Initializes the virtual memory management system and returns a MemoryManagementInfo instance, which represents the initial (kernel) address space. "]]});
